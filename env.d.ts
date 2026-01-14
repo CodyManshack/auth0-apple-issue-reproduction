143ec7e9be29c00bc7a2c1c97bedbdfc71aedc88
Cody Manshack <cody.manshack@gmail.com>
2026-01-14 13:23:59 +0100

    add keys

--- env.d.ts
+++ env.d.ts
@@ -3,6 +3,9 @@
 interface ImportMetaEnv {
     readonly VITE_AUTH0_DOMAIN: string
     readonly VITE_AUTH0_CLIENT_ID: string
+    readonly VITE_AUTH0_AUDIENCE: string
+    readonly VITE_APP_ID: string
+    readonly VITE_APP_DOMAIN: string
 }
 
 interface ImportMeta {
